fdb5d4c63e9a81744c9b23dd92a330f4a80eaeff
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-11 18:38:39 +0100

    react-hook-forms package installed

--- ShopVerse/src/pages/Auth.jsx
+++ ShopVerse/src/pages/Auth.jsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
+
+const formGroupStyles = "flex flex-col gap-1 mb-4";
+const buttonStyles =
+  "py-2 px-4 mb-2 bg-accent rounded-md text-text font-semibold cursor-pointer active:scale-95 shadow-lg transition-all duration-300 hover:translate-y-[-2px]";
+const labelStyles = "text-text text-lg font-semibold tracking-wider";
+const inputStyles =
+  "border-2 text-text border-border rounded-md py-1 px-2 outline-none transition-colors duration-300 focus:ring-2 focus:ring-accent";
+
 const Auth = () => {
-  const groupDataStyles = "flex flex-col gap-1 mb-4";
-  const buttonStyles =
-    "py-2 px-4 mb-2 bg-accent rounded-md text-text font-semibold cursor-pointer active:scale-95 shadow-lg transition-all duration-300 hover:translate-y-[-2px]";
-  const labelStyles = "text-text text-lg font-semibold tracking-wider";
-  const inputStyles =
-    "border-2 text-text border-border rounded-md py-1 px-2 outline-none transition-colors duration-300 focus:ring-2 focus:ring-accent";
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="w-full max-w-sm bg-surface rounded-xl p-8 shadow-lg">
-        <h1 className="text-2xl font-bold text-text mb-4">Sign-up Page</h1>
+      <div className="w-full max-w-sm bg-surface rounded-xl p-8 shadow-2xl">
+        <h1 className="text-2xl font-bold text-text mb-4">Signup Page</h1>
         <form>
-          <div className={groupDataStyles}>
+          <div className={formGroupStyles}>
             <label htmlFor="email" className={labelStyles}>
               Email
             </label>
@@ -22,7 +25,7 @@ const Auth = () => {
               placeholder="Enter your email here..."
             />
           </div>
-          <div className={groupDataStyles}>
+          <div className={formGroupStyles}>
             <label htmlFor="password" className={labelStyles}>
               Password
             </label>
@@ -38,10 +41,13 @@ const Auth = () => {
             Sign-up
           </button>
           <p className="text-text">
-            Already have an account with us?
-            <span className="text-accent font-semibold ml-1 underline cursor-pointer">
+            Already have an account?
+            <Link
+              to="/auth"
+              className="text-accent font-semibold ml-1 underline cursor-pointer"
+            >
               Login
-            </span>
+            </Link>
           </p>
         </form>
       </div>
