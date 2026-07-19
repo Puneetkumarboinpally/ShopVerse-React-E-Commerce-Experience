97b744f9b719ba88a9d3428c8ea8a26a4a0e355f
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-20 00:08:13 +0100

    comments added for readability

--- ShopVerse/src/pages/Cart.jsx
+++ ShopVerse/src/pages/Cart.jsx
@@ -3,21 +3,30 @@ const Cart = () => {
     <div className="text-red-500 flex justify-center items-center min-h-screen">
       <div>
         <h1>Checkout</h1>
+        {/* products section */}
         <div className="products w-1/2 flex justify-between items-center">
+          {/* products cart section */}
+
           <div className="card ">
+            {/*product cart image section */}
             <div>
               <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500&h=500&fit=crop" />
             </div>
+            {/* product cart details */}
             <div>
               <h2>wireless HeadPhones</h2>
               <p>$99.99 each</p>
             </div>
+            {/* products cart buttons section */}
+
             <div>
+              {/* buttons section */}
               <div>
                 <button>-</button>
                 <p>10</p>
                 <button>+</button>
               </div>
+              {/* total price */}
               <p>$999.9</p>
               <button>Remove</button>
             </div>
