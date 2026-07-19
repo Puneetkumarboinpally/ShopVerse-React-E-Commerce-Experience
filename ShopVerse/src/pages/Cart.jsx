618febdaecd3cae2b05c0f9996fed875bff94b7c
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-19 23:58:28 +0100

    checkout products layout added

--- ShopVerse/src/pages/Cart.jsx
+++ ShopVerse/src/pages/Cart.jsx
@@ -1,7 +1,32 @@
 const Cart = () => {
   return (
-    <div className="text-red-500 min-h-screen">Cart</div>
-  )
-}
+    <div className="text-red-500 flex justify-center items-center min-h-screen">
+      <div>
+        <h1>Checkout</h1>
+        <div className="products">
+          <div className="card w-1/2">
+            <div>
+              <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500&h=500&fit=crop" />
+            </div>
+            <div>
+              <h2>wireless HeadPhones</h2>
+              <p>$99.99 each</p>
+            </div>
+            <div>
+              <div>
+                <button>-</button>
+                <p>10</p>
+                <button>+</button>
+              </div>
+              <p>$999.9</p>
+              <button>Remove</button>
+            </div>
+          </div>
+        </div>
+        <div className="total"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
