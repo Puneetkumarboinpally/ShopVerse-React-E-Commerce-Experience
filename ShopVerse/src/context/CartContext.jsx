862dd340e7a7a8318ce91da42a93da3e7265cf25
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-07-22 20:20:07 +0100

    addtoCart funtionality added

--- ShopVerse/src/context/CartContext.jsx
+++ ShopVerse/src/context/CartContext.jsx
@@ -1,14 +1,38 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext(null);
 
-const CartProvider = ({ Children }) => {
-  const addToCart = () => {
-    console.log("added");
+const CartProvider = ({ children }) => {
+  const [cartItem, setCartItem] = useState([]);
+
+  //
+  const addToCart = (productId) => {
+    // find the existing product from the cart
+    const existing = cartItem.find((item) => item.id === productId);
+
+    // if existing product found
+    if (existing) {
+      // take current quantity from the existing product
+      const currentQuantity = existing.quantity;
+
+      // and map it to a new array with only increasing the the quantity while keeping the items same
+      const updatedCartQuantity = cartItem.map((item) =>
+        item.id === productId
+          ? { id: productId, quantity: currentQuantity + 1 }
+          : item,
+      );
+      setCartItem(updatedCartQuantity);
+    } else {
+      setCartItem([...cartItem, { id: productId, quantity: 1 }]);
+    }
   };
   return (
-    <CartContext.Provider value={addToCart}>{Children}</CartContext.Provider>
+    <CartContext.Provider value={{ addToCart }}>
+      {children}
+    </CartContext.Provider>
   );
 };
+
+export const useCart = () => useContext(CartContext);
 
 export default CartProvider;
